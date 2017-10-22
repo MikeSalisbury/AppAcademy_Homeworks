782c275c7316bd67946dcf7800aa2cdba7ee48b2
Mike Salisbury <michael.salisbury87@gmail.com>
2017-10-21 22:28:21 -0700

    book controller exercises

---
+++ Week 4/W4D1/Controllers/app/controllers/books_controller.rb
@@ -0,0 +1,36 @@
+class BooksController < ApplicationController
+  def index
+    # your code here
+    @books = Book.all
+    render :index
+  end
+
+  def new
+    # your code here
+    render :new
+  end
+
+  def create
+    # your code here
+    book = Book.new(book_params)
+    if book.save
+      redirect_to books_url
+    else
+      render book.errors.to_a, status: 200
+      render :new
+    end
+
+  end
+
+  def destroy
+    # your code here
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
